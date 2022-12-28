06a7020ba8ca2de832c9b10c511acaf2a80be57b
Artur Bartosik <luafanti@gmail.com>
2022-12-28 08:10:46 +0100

    Update to Spring Boot 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.6"
+    id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.luafanti"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
@@ -34,7 +34,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
--- infra/docker/build.sh
+++ infra/docker/build.sh
@@ -1,7 +1,6 @@
-#/bin/sh
+#!/bin/sh
 
 set -e
 
 ./gradlew --no-daemon --info bootJar --exclude-task test
-
 mv $(./gradlew --no-daemon --quiet bootJarPath) app.jar
