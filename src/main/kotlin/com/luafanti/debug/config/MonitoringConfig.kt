ca6697742cfcf156c42b47131951e6ad7d3979eb
Artur Bartosik <luafanti@gmail.com>
2022-12-29 10:59:56 +0100

    Custom tags for Micrometer metrics

---
+++ src/main/kotlin/com/luafanti/debug/config/MonitoringConfig.kt
@@ -0,0 +1,22 @@
+package com.luafanti.debug.config
+
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class MetricConfig {
+
+    @Value("\${spring.application.name}")
+    private val appName: String? = null
+
+    @Bean
+    fun commonTags(): MeterRegistryCustomizer<MeterRegistry>? {
+        return MeterRegistryCustomizer { registry: MeterRegistry ->
+            registry.config().commonTags("application", appName)
+        }
+    }
+}
